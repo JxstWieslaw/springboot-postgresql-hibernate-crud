3140e789f9e787e6dc4b8f50338bb898893cc958
JxstWieslaw <wieslawsamushonga01@gmail.com>
2022-02-06 21:22:40 +0530

    Initial Commit

---
+++ src/main/java/ml/codevilla/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package ml.codevilla.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ml.codevilla.springboot.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
